664139656c164ca5e1cebb0f15ecf8b3c980874f
Celina Rabe <crabe@epionhealth.com>
2021-12-18 13:34:23 -0600

    added rjavascript pack tags

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import Home from "../components/Home"
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
 
-document.addEventListener("DOMContentLoaded", () => {
-  render(
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import Home from '../src/Home'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
     <Home />,
-    document.body.appendChild(document.createElement("div"))
-  );
-});
+    document.body.appendChild(document.createElement('div')),
+  )
+})
